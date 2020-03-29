2c9685a3f5dbe1198b2cf05400d30e8fd7b10578
Mohammad Jouza <m.jozaa@yahoo.com>
2020-03-29 07:19:30 +0300

    Updates

---
+++ precache-manifest.8f2751ce115c2ced82c15dadab2e47f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f31ee45b0b243f657088e3df07bf1b",
+    "url": "/ShortPoint/index.html"
+  },
+  {
+    "revision": "14ab8e088cff93f6d62b",
+    "url": "/ShortPoint/static/css/main.3a829338.chunk.css"
+  },
+  {
+    "revision": "5008dc51417bd79f997d",
+    "url": "/ShortPoint/static/js/2.fa8d609a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShortPoint/static/js/2.fa8d609a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ab8e088cff93f6d62b",
+    "url": "/ShortPoint/static/js/main.b5cad950.chunk.js"
+  },
+  {
+    "revision": "cfbce348b4fc205d73b6",
+    "url": "/ShortPoint/static/js/runtime-main.c2c6007f.js"
+  }
+]);
